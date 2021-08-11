935ef8c02d0847ffc5f5b9c2195a6cc8f2a23bdd
Natallia Mikhalenak <iam@natallia.dev>
2021-08-11 22:54:06 +0300

    Encrypt the user password in DB

--- init_db.js
+++ init_db.js
@@ -3,19 +3,9 @@ import { PERMISSIONS } from './src/utils/constants';
 
 const users = [{
     id: '0df71432-7b42-4eb3-bb11-def8334c4101',
-    login: 'natallia',
-    password: 'Secret1',
+    login: 'Admin',
+    password: 'f42de9fe905b0376af70106ee8354e988c7013afd0de095dcaccf68cadab61ac4066293f0991cd325b9ab3c6a75c7878e658e904dff6b6d085719511f7c752db',
     age: 18
-}, {
-    id: '3dd71432-7b42-4eb3-bb11-def8334c5101',
-    login: 'olga',
-    password: 'qwerty',
-    age: 19
-}, {
-    id: '5c563ad5-eedc-40c1-a0fb-a96f921804ae',
-    login: 'alex',
-    password: 'newpassword',
-    age: 20
 }];
 
 const groups = [{
